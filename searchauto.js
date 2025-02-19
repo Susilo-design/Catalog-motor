9e775ef5110069f0e7957a72f9b7d3d6ecc83b0c
FarelH <farelmantap456@gmail.com>
2025-02-19 12:57:13 +0700

    preview card + remake search engine (unfinished)

---
+++ searchauto.js
@@ -0,0 +1,37 @@
+// let availableKeywords = [
+//     'Ducati Panigale V4S',
+//     'Kawasaki H2'
+// ];
+
+// const resultsBox = document.querySelector(".result-box");
+// const inputBox = document.getElementById("input-box");
+
+// inputBox.onkeyup = function() {
+//     let result = [];
+//     let input = inputBox.value;
+//     if (input.length) {
+//         result = availableKeywords.filter((keyword) => {
+//             return keyword.toLowerCase().includes(input.toLowerCase());
+//         });
+//         console.log(result)
+//     }
+//     display(result)
+
+//     if(!result.length) {
+//         resultsBox.innerHTML = '';
+//     }
+// }
+
+// function display(result){
+//     const content = result.map((list) => {
+//         return `<li class="data-list" data-name="p-1" onclick=selectorInput(this)>` + list + `</li>`;
+//     });
+
+//     resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+// }
+
+// function selectInput(list){
+//     inputBox.value = list.innerHTML;
+//     resultsBox.innerHTML = '';
+// }
+
